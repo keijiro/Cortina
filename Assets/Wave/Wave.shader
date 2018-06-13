2cc45fc937b2f87dd896fb461f84e9785bbba3cf
Keijiro Takahashi <keijiro@gmail.com>
2018-06-13 09:37:35 +0900

    Wave effect

---
+++ Assets/Wave/Wave.shader
@@ -0,0 +1,54 @@
+﻿Shader "Hidden/Cortina/Wave"
+{
+    Properties
+    {
+        _MainTex("", 2D) = "black"
+    }
+
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+    #include "Assets/Common/Shaders/SimplexNoise3D.hlsl"
+
+    sampler2D _MainTex;
+    float4 _MainTex_TexelSize;
+
+    half _Distortion;
+    half _Feedback;
+
+    half4 FragmentFeedback(v2f_img input) : SV_Target
+    {
+        const float aspect = _MainTex_TexelSize.x * _MainTex_TexelSize.w;
+        float3 np = float3(input.uv * float2(aspect, 1) * 5, _Time.y * 0.75);
+        float2 nf = snoise_grad(np).xy * _Distortion;
+        float2 delta = cross(float3(nf, 0), float3(0, 0, 1));
+        float src = tex2D(_MainTex, input.uv + delta * float2(aspect, 1)).r;
+        return src * _Feedback;
+    }
+
+    half4 FragmentBlit(v2f_img input) : SV_Target
+    {
+        return tex2D(_MainTex, input.uv).r;
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment FragmentFeedback
+            ENDCG
+        }
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment FragmentBlit
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Wave/WaveEffect.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+namespace Cortina
+{
+    sealed class WaveEffect : MonoBehaviour
+    {
+        #region Public properties
+
+        [SerializeField, Range(0, 0.1f)] float _distortion = 0.05f;
+
+        public float Distortion {
+            get { return _distortion; }
+            set { _distortion = value; }
+        }
+
+        [SerializeField, Range(0, 1)] float _feedback = 1;
+
+        public float Feedback {
+            get { return _feedback; }
+            set { _feedback = value; }
+        }
+
+        [SerializeField] bool _hold = true;
+
+        public bool Hold {
+            get { return _hold; }
+            set { _hold = value; }
+        }
+
+        #endregion
+
+        #region Built-in resources
+
+        [SerializeField, HideInInspector] Shader _shader;
+        Material _sheet;
+
+        #endregion
+
+        #region Internal objects
+
+        RenderTexture _rt1;
+        RenderTexture _rt2;
+
+        #endregion
+
+        #region MonoBehaviour implementation
+
+        void Start()
+        {
+            _sheet = new Material(_shader);
+            _rt1 = new RenderTexture(1920, 640, 0, RenderTextureFormat.RHalf);
+            _rt2 = new RenderTexture(1920, 640, 0, RenderTextureFormat.RHalf);
+        }
+
+        void OnDestroy()
+        {
+            Destroy(_sheet);
+            Destroy(_rt1);
+            Destroy(_rt2);
+        }
+
+        void Update()
+        {
+            // Feedback
+            _sheet.SetFloat("_Distortion", _distortion);
+            _sheet.SetFloat("_Feedback", _feedback);
+            Graphics.Blit(_rt1, _rt2, _sheet, 0);
+
+            // Double buffering
+            var temp = _rt1;
+            _rt1 = _rt2;
+            _rt2 = temp;
+        }
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (_hold) Graphics.Blit(source, _rt1, _sheet, 0);
+            Graphics.Blit(_rt1, destination, _sheet, 1);
+        }
+
+        #endregion
+    }
+}
