76fc76f91ddc27499ef6484fba373d9aebbf6095
Keijiro Takahashi <keijiro@gmail.com>
2018-06-14 13:13:44 +0900

    Add FogController

---
+++ Assets/Common/Scripts/FogController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FogController : MonoBehaviour
+{
+    [SerializeField] float _density = 0;
+
+    void LateUpdate()
+    {
+        if (_density < 0.0001f)
+        {
+            RenderSettings.fog = false;
+        }
+        else
+        {
+            RenderSettings.fog = true;
+            RenderSettings.fogDensity = _density;
+        }
+    }
+}
